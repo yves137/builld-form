80ac1a940ea5dda12d0ec9a4794421c1bba4a4b9
yves137 <oneg2981@gmail.com>
2023-09-27 14:42:50 +0200

    update form

--- src/Displayer.js
+++ src/Displayer.js
@@ -13,8 +13,8 @@ export function Displayer(){
     }
    
     return <div className='flex flex-col items-center space-y-5'>
-            <input type='text' value={firstName}  className='text-black text-xl border py-5 px-10 rounded-md' onChange={(e)=>setFirstName(v=>e.target.value)}/>
-            <input type='text' value={lastName}  className='text-black text-xl border py-5 px-10 rounded-md' onChange={(e)=>setLastName(v=>e.target.value)}/>
+            <input type='text' value={firstName} placeholder='Fisrt name'  className='text-black text-xl border py-5 px-10 rounded-md' onChange={(e)=>setFirstName(v=>e.target.value)}/>
+            <input type='text' value={lastName} placeholder='Last name'  className='text-black text-xl border py-5 px-10 rounded-md' onChange={(e)=>setLastName(v=>e.target.value)}/>
             <CustomButtons handleClick={handleClick} />
         </div>
 }
